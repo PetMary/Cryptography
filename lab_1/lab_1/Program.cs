f74c7776f9e524124baa628509711e34587603d3
PetMary <m_petrova01@mail.ru>
2022-05-15 13:40:11 +0300

    1.1 и 1.2

---
+++ lab_1/lab_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ps_block ps = new ps_block();
+            byte[] array_byte = {24, 91, 17, 5 };
+            int[,] p_block ={
+                { 16,  7, 20, 21, 29, 12, 28, 17 },
+                { 1, 15, 23, 26,  5, 18, 31, 10 },
+                { 2,  8, 24, 14, 32, 27,  3,  9 },
+                { 19, 13, 30,  6, 22, 11,  4, 25 }};
+
+            var res = ps.P_Block(array_byte, p_block);
+            Console.WriteLine("P-перестановка:");
+            for (int i = 0; i < res.Length; i++)
+            {
+                Console.WriteLine("({0}){1} = {2}", i+1, res[i], Convert.ToString(res[i], 2));
+            }
+
+            byte[] array_byte2 = { 102, 91, 217};
+            int[] s_block_4 = { 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7 };
+            int[] s_block_3 = { 7, 4, 1, 0, 5, 3, 2, 6 };
+            int k = 3;
+
+            var res2 = ps.S_Block(array_byte2, s_block_3, k);
+            Console.WriteLine("S-перестановка:");
+            for (int i = 0; i < res2.Length; i++)
+            {
+                Console.WriteLine("({0}){1} = {2}", i + 1, res2[i], Convert.ToString(res2[i], 2));
+            }
+
+        }
+    }
+}
---
+++ lab_1/lab_1/ps_block.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1
+{
+    class ps_block
+    {
+        public ps_block() { }
+
+        public byte[] P_Block(byte[] array_byte, int[,] p_block)
+        {
+            int size_i = p_block.GetUpperBound(0) + 1;
+            int size_j = p_block.GetUpperBound(1) + 1;
+            byte[] res = new byte[size_i];
+
+            if (p_block is null)
+                throw new ArgumentException("p-block is null");
+
+            for (int i = 0; i < size_i; i++)
+            {
+                for (int j = 0; j < size_j; j++)
+                {
+                    int index = p_block[i, j] - 1;
+                    byte byte_x = array_byte[index / size_j];
+                    byte x = (byte)((byte_x >> (size_j - 1 - index % size_j)) & 1);
+                    res[i] = (byte)(res[i] | (x << (size_j - 1 - j)));
+                }
+            }
+            return res;
+        }
+
+        public byte[] S_Block(byte[] array_byte, int[] s_block, int k)
+        {
+            byte[] res = new byte[array_byte.Length];
+            byte new_byte = 0, k_bit = 0;
+
+            if (s_block is null)
+                throw new ArgumentException("s-block is null");
+
+            for (int i = array_byte.Length - 1, j = 0; i >= 0; i--)
+            {
+                for (; j < 8; j += k)
+                {
+                    k_bit = (byte)((array_byte[i] >> j) & ((1 << k) - 1));
+                    if (j + k <= 8)
+                        new_byte = (byte)(new_byte | (s_block[k_bit] << j));
+                }
+
+                if ((j != 8) && (i != 0))
+                {
+                    k_bit = (byte)(k_bit | ((array_byte[i - 1] & ((1 << (j - 8)) - 1)) << (k - j + 8)));
+                    new_byte = (byte)((new_byte | (s_block[k_bit] << (j - k))) & 255);
+
+                }
+                else if ((i == 0) && (j != 8))
+                {
+                    new_byte = (byte)(new_byte | (k_bit << (j - k)));
+                }
+
+                res[i] = new_byte;
+                new_byte = (byte)(s_block[k_bit] >> (k - j + 8));
+                j = j % 8;
+            }
+            return res;
+        }
+    }
+}
